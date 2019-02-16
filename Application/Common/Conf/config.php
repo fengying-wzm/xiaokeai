c8d8e33386a816f528300689c541abb63391fd31
wuzhimin <297083979@qq.com>
2019-02-16 15:07:55 +0800

    更改数据库用户名

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -5,7 +5,7 @@ return array(
 	'DB_TYPE'   => 'mysql', // 数据库类型
 	'DB_HOST'   => 'localhost', // 服务器地址
 	'DB_NAME'   => 'gethot', // 数据库名
-	'DB_USER'   => 'root', // 用户名
+	'DB_USER'   => 'admin', // 用户名
 	'DB_PWD'    => '123456', // 密码
 	'DB_PORT'   => 3306, // 端口
 	'DB_PREFIX' => 'gh_', // 数据库表前缀 
